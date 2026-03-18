dfae954aa4a35840832011dc26305a73872cba0a
x1emafra <x1emafra@gmail.com>
2026-03-18 00:51:01 -0300

    listen 0.0.0.0:

--- server.js
+++ server.js
@@ -49,6 +49,8 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {  // + '0.0.0.0'
   console.log(`Server running on port ${PORT}`);
+});
+
 });
